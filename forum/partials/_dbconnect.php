b213d2464cdd29fe3d650db4ee5255b819e39c64
ujjawla suresh nimbolkar <102054495+ujjawala123@users.noreply.github.com>
2024-06-27 12:03:27 +0530

    Delete forum directory

--- forum/partials/_dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Script to connect to the database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "idiscuss";
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
